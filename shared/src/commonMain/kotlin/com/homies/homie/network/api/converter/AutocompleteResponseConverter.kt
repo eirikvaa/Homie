d485c3ef0a6fb5ca0c815eff452243dea8e18be1
Eirik Vale Aase <e.v.aase@gmail.com>
2022-02-15 18:35:37 +0100

    Move coordinates validation to class constructor

--- shared/src/commonMain/kotlin/com/homies/homie/network/api/converter/AutocompleteResponseConverter.kt
+++ shared/src/commonMain/kotlin/com/homies/homie/network/api/converter/AutocompleteResponseConverter.kt
@@ -8,21 +8,6 @@ internal fun AutocompleteResponse.asLocations(): List<Location> = features.map {
     Location(
         name = it.properties.name,
         place = it.properties.id,
-        coordinates = it.geometry.coordinates.asCoordinates()
-    )
-}
-
-// This extension is only used in this file, so let's keep it here for now.
-internal fun List<Float>.asCoordinates(): Coordinates? {
-    // We need a latitude and a longitude, nothing more or less.
-    if (size != 2) {
-        return null
-    }
-
-    // The Entur API dictates that the first element is the longitude and the second element
-    // is the latitude, even though it should be the other way around.
-    return Coordinates(
-        latitude = this[1],
-        longitude = this[0]
+        coordinates = Coordinates(values = it.geometry.coordinates)
     )
 }
