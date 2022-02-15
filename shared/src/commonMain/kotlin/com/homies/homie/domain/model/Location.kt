287d1110e598f73c3bd94e128a60fa5db18c2678
Eirik Vale Aase <e.v.aase@gmail.com>
2022-02-15 14:33:40 +0100

    Add domain models

---
+++ shared/src/commonMain/kotlin/com/homies/homie/domain/model/Location.kt
@@ -0,0 +1,7 @@
+package com.homies.homie.domain.model
+
+data class Location(
+    val name: String?,
+    val place: String?,
+    val coordinates: Coordinates?
+)
---
+++ shared/src/commonMain/kotlin/com/homies/homie/domain/model/TripPattern.kt
@@ -0,0 +1,6 @@
+package com.homies.homie.domain.model
+
+data class TripPattern(
+    val expectedStartTime: String?,
+    val expectedEndTime: String?
+)
