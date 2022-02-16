e8c88ec7a5c383babee3a6da80d5f147c7707379
Eirik Vale Aase <e.v.aase@gmail.com>
2022-02-16 08:08:52 +0100

    Turn KDoc comment into regular comment

--- shared/src/commonMain/kotlin/com/homies/homie/network/api/converter/AutocompleteResponseConverter.kt
+++ shared/src/commonMain/kotlin/com/homies/homie/network/api/converter/AutocompleteResponseConverter.kt
@@ -9,11 +9,9 @@ internal fun AutocompleteResponse.asLocations(): List<Location> = features.map {
         name = it.properties.name,
         place = it.properties.id,
         coordinate = Coordinate(
-            /**
-             * From the Entur Geocoder API we get coordinates in the form of a list of values.
-             * The first element needs to be the longitude and the second the latitude, even though it
-             * should be the other way around.
-             */
+            // From the Entur Geocoder API we get coordinates in the form of a list of values.
+            // The first element needs to be the longitude and the second the latitude, even though it
+            // should be the other way around.
             longitude = it.geometry.coordinates[1],
             latitude = it.geometry.coordinates[0]
         )
