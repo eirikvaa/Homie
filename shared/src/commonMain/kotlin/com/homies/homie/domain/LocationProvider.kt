18154b1755aa4bd1214cf5e01ba69877beb1a3db
Eirik Vale Aase <e.v.aase@gmail.com>
2022-07-10 02:46:54 +0200

    Suppress wrong no actual warning

--- shared/src/commonMain/kotlin/com/homies/homie/domain/LocationProvider.kt
+++ shared/src/commonMain/kotlin/com/homies/homie/domain/LocationProvider.kt
@@ -3,6 +3,11 @@ package com.homies.homie.domain
 import com.homies.homie.domain.model.Coordinate
 import kotlinx.coroutines.flow.Flow
 
+// TODO:
+//  We really shouldn't be using this, but I think there is an IDE
+//  bug that causes the JVM declaration to be not found. Hopefully
+//  we can remove this when we upgrade the Kotlin version.
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 expect interface LocationProvider {
     suspend fun getLocationUpdates(): Flow<Coordinate>
 }
